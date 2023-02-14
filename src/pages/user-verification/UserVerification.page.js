3012758db9a181087aad1a232f2508730b476220
Shawn Kebel <fxrender@outlook.com>
2023-02-14 14:11:27 -0600

    :sparkles: Add `userVerification` page

---
+++ src/pages/user-verification/UserVerification.page.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { Alert, Jumbotron, Spinner } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { userRegistrationVerification } from "../../api/userApi";
+
+import "./userVerification.style.css";
+
+const initialResponse = {
+  status: "",
+  message: "",
+};
+
+export const UserVerification = () => {
+  const { _id, email } = useParams();
+  const [response, setResponse] = useState(initialResponse);
+
+  useEffect(() => {
+    const apiCall = async () => {
+      const dta = { _id, email };
+      const result = await userRegistrationVerification(dta);
+      setResponse(result);
+    };
+
+    !response.status && apiCall();
+  }, [response, _id, email]);
+
+  // call api and send the _id to verify user
+  return (
+    <div className="registration-page bg-info">
+      <div className="mt-5">
+        <Jumbotron className="form-box">
+          {!response.status && <Spinner variant="info" animation="border" />}
+          {response.status && (
+            <Alert
+              variant={response.status === "success" ? "success" : "danger"}
+            >
+              {response.message}
+            </Alert>
+          )}
+        </Jumbotron>
+      </div>
+    </div>
+  );
+};
